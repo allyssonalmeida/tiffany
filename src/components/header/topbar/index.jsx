844b26610841f3c2f357f1e3f793ffd1d05ec573
Allysson Almeida <allyssonif@gmail.com>
2023-06-01 12:40:23 -0300

    feat: update slider and add product shelf

--- src/components/header/topbar/index.jsx
+++ src/components/header/topbar/index.jsx
@@ -1,27 +1,28 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
-import '@splidejs/react-splide/css/core';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import style from '../../../styles/Header.module.scss'
 
 const options = {
-  type: 'loop',
-  perPage: 1,
-  pagination: false,
-  autoplay: true,
-  interval: 3000
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 1000,
+  slidesToShow: 1,
+  slidesToScroll: 1
 }
 
 const TopBar = () => {
   return (
-    <Splide options={options} className={style.topBar}>
-      <SplideSlide className={style.topBar__text}>
+    <Slider {...options} className={style.topBar}>
+      <div className={style.topBar__text}>
         <p>Order now from complimentary delivery and returns on all orders.</p>
-      </SplideSlide>
-      <SplideSlide className={style.topBar__text}>
+      </div>
+      <div className={style.topBar__text}>
         <p>Our graduation gifts are the perfect way to say {'congratulations'}. <a href="#">Shop now</a></p>
-      </SplideSlide>
-    </Splide>
+      </div>
+    </Slider>
   )
 }
 
--- src/components/shelf/index.jsx
+++ src/components/shelf/index.jsx
@@ -1,22 +1,18 @@
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
-import '@splidejs/react-splide/css/core';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import style from '../../styles/Shelf.module.scss'
 import items from '../../data/products.json'
 
 
-const shelfOptions = {
-  type: 'slide',
-  perPage: 4,
-  perMove: 1,
-  pagination: false,
-}
-const galleryOptions = {
-  type: 'fade',
-  perPage: 1,
-  pagination: true,
-  arrows: false
+const options = {
+  dots: false,
+  infinite: false,
+  arrows: true,
+  speed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 1
 }
 
 const Shelf = () => {
@@ -28,23 +24,23 @@ const Shelf = () => {
     <section className={style.shelf}>
       {
         products &&
-        <Splide options={shelfOptions}>
+        <Slider {...options}>
           {products.map((product, index) => (
-            <SplideSlide key={index} className={style.product__wrapper}>
-              <a className={style.product}>
-                <Splide className={style.product__gallery} options={galleryOptions}>
-                  {
-                    product.images.map((image, index) => (
-                      <SplideSlide key={index} className={style.product__image}>
-                        <img src={image.url} alt={image.alt} />
-                      </SplideSlide>
-                    ))
-                  }
-                </Splide>
+            <div key={index} className={style.product}>
+              <a href="#product" key={index} className={style.product__image}>
+                <img src={product.images[0].url} alt={product.images[0].alt} />
               </a>
-            </SplideSlide>
+              <div className={style.product__summary}>
+                <strong className={style.product__name}></strong>
+                <button className={style.product__cta}>
+                  <span className={style.product__price}>
+                    {(product.price / 100).toLocaleString('en-US', { type: 'currency', currency: 'USD' })}
+                  </span>
+                </button>
+              </div>
+            </div>
           ))}
-        </Splide>
+        </Slider>
       }
     </section>
   )
